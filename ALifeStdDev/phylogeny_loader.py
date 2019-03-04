a182be27de4bf0c8c3c38a32147dd9c4f485e2f4
Emily Dolson <EmilyLDolson@gmail.com>
2019-03-04 16:51:30 -0500

    Fix accidental keyword clobbering

--- ALifeStdDev/phylogeny_loader.py
+++ ALifeStdDev/phylogeny_loader.py
@@ -4,12 +4,12 @@ import ast
 
 def load_phylogeny_to_networkx(filename):
     """
-    Loads a phylogeny in standards format (in csv or json) from the file 
-    specified by the filename parameter. Returns the phylogeny as a 
+    Loads a phylogeny in standards format (in csv or json) from the file
+    specified by the filename parameter. Returns the phylogeny as a
     networkx digraph (with edges going from parent to child).
 
     Special ancestor values are encoded in an "origin" field within nodes.
-    Example: `my_phylogeny.nodes["A"]["origin"]` would return the origin of 
+    Example: `my_phylogeny.nodes["A"]["origin"]` would return the origin of
     node "A"
     """
     phylogeny = nx.DiGraph()
@@ -18,27 +18,27 @@ def load_phylogeny_to_networkx(filename):
         data = pd.read_csv(filename)
         data.loc[:,"ancestor_list"] = data.loc[:,"ancestor_list"].apply(ast.literal_eval)
         data.set_index("id", inplace=True)
-    elif filename.endswith(".json"):    
+    elif filename.endswith(".json"):
         data = pd.read_json(filename, orient="index", convert_dates=False)
 
     # print(data)
 
     # Have to do this in two passes, (one to add nodes, one to
     # add eges) because order in file is not required/guaranteed
-    for id in data.index:
-        phylogeny.add_node(id)
+    for taxon_id in data.index:
+        phylogeny.add_node(taxon_id)
 
-    for id in data.index:
+    for taxon_id in data.index:
 
-        for ancestor in data.loc[id, "ancestor_list"]:
+        for ancestor in data.loc[taxon_id, "ancestor_list"]:
             try:
                 ancestor = int(ancestor)
                 if phylogeny.has_node(ancestor):
-                    phylogeny.add_edge(ancestor, id)        
+                    phylogeny.add_edge(ancestor, taxon_id)        
                 else:
-                    raise Exception(f"{id}'s ancestor, {ancestor}, is not in this file.")
+                    raise Exception(f"{taxon_id}'s ancestor, {ancestor}, is not in this file.")
 
             except ValueError as e:
-                phylogeny.nodes[id]["origin"] = ancestor
+                phylogeny.nodes[taxon_id]["origin"] = ancestor
 
     return phylogeny
