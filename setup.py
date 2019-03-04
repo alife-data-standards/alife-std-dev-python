da038bc4e7ba96c233284ea9ef2232078ee5d66f
Alex Lalejini <amlalejini@gmail.com>
2019-03-04 13:00:26 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ALifeStdDev',
+      version='0.1',
+      description='Python development tools for working with standardized ALife data.',
+      url='https://github.com/alife-data-standards/alife-std-dev-python',
+      author='Emily Dolson, Alex Lalejini',
+      author_email='dolsonem@msu.edu, lalejini@msu.edu',
+      license='MIT',
+      packages=['ALifeStdDev'],
+zip_safe=False)
