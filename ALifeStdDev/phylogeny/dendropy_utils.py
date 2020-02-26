f21a63d70077345441b9b52c3f470f5dca1127c1
Emily Dolson <emilyldolson@gmail.com>
2020-02-26 16:28:34 -0500

    Start working on utils to make use of dendropy

---
+++ ALifeStdDev/phylogeny/dendropy_utils.py
@@ -0,0 +1,54 @@
+import dendrodpy
+import pandas as pd
+import pd
+
+def load_phylogeny_to_dendropy(filename):
+    """
+    Loads a phylogeny in standards format (in csv or json) from the file
+    specified by the filename parameter. Returns the phylogeny as a
+    dendropy tree.
+
+    Special ancestor values are encoded in an "origin" field within nodes.
+    Example: `my_phylogeny.nodes["A"]["origin"]` would return the origin of
+    node "A"
+    """
+
+    if filename.endswith(".csv"):  # Handle CSV files
+        data = pd.read_csv(filename)
+        # The ancestor_list column contains a string of lists of strings
+        # Let's turn it into an actual list of strings
+        data.loc[:, "ancestor_list"] = \
+            data.loc[:, "ancestor_list"].apply(ast.literal_eval)
+        data.set_index("id", inplace=True)
+    elif filename.endswith(".json"):  # Handle JSON files
+        data = pd.read_json(filename, orient="index", convert_dates=False)
+
+    # Have to do this in two passes, (one to add nodes, one to
+    # add eges) because order in file is not required/guaranteed
+
+    taxon_namespace = dendropy.TaxonNamespace(data.index)
+    phylogeny = dendropy.Tree(taxon_namespace=taxon_namespace)
+
+    # Add nodes
+    # for taxon_id in data.index:
+    #     phylogeny.add_node(taxon_id)
+    #     for col in data:
+    #         if col == "id":
+    #             continue
+    #         phylogeny.nodes[taxon_id][col] = data.loc[taxon_id, col]
+
+    # # Add edges
+    # for taxon_id in data.index:
+
+    #     for ancestor in data.loc[taxon_id, "ancestor_list"]:
+    #         try:  # Conversion to int will fail if its a special value
+    #             ancestor = int(ancestor)
+    #             if phylogeny.has_node(ancestor):
+    #                 phylogeny.add_edge(ancestor, taxon_id)
+    #             else:
+    #                 raise Exception(f"{taxon_id}'s ancestor, {ancestor}, is not in this file.")
+
+    #         except ValueError:  # Handle special values
+    #             phylogeny.nodes[taxon_id]["origin"] = ancestor
+
+    # return phylogeny
