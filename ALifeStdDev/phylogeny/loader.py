9c6ec83d64a0f63bd930a5bf03b5814eefd70898
Emily Dolson <emilyldolson@gmail.com>
2023-03-03 20:10:14 -0500

    fix whitespace handlig

--- ALifeStdDev/phylogeny/loader.py
+++ ALifeStdDev/phylogeny/loader.py
@@ -5,8 +5,12 @@ import ast
 
 def load_phylogeny_to_pandas_df(filename):
     data = pd.read_csv(filename)
+    data.columns = data.columns.str.replace(' ', '')
+
     # The ancestor_list column contains a string of lists of strings
     # Let's turn it into an actual list of strings
+    data.loc[:, "ancestor_list"] = \
+        data.loc[:, "ancestor_list"].apply(lambda x: x.strip())
     data.loc[:, "ancestor_list"] = \
         data.loc[:, "ancestor_list"].apply(ast.literal_eval)
     data.set_index("id", inplace=True)
@@ -69,7 +73,11 @@ def networkx_to_pandas_df(g, bonus_cols=None):
         bonus_cols = {}
     df = pd.DataFrame()
     df["id"] = g.nodes.keys()
-    df["ancestor_list"] = [list(g.predecessors(i)) for i in df["id"]]
+    df["ancestor_list"] = [list(g.predecessors(i)) if len(list(g.predecessors(i))) > 0 else [None] for i in df["id"]]
+
+    # roots = df[df["ancestor_list"].apply(lambda x: len(x) == 0)].index
+    # df.loc[roots, "ancestor_list"] = "[None]"
+
     for name1, name2 in bonus_cols.items():
         df[name1] = [g.nodes[i][name2] for i in df["id"]]
 
